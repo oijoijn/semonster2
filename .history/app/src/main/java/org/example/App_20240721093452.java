a28d8701ed73082f1974123742976d58aba294c3
repllca <maridann1022@icloud.com>
2024-07-21 09:37:52 +0900

    change Greeting message

--- .history/app/src/main/java/org/example/App_20240721093452.java
+++ .history/app/src/main/java/org/example/App_20240721093452.java
@@ -6,7 +6,7 @@ package org.example;
 public class App {
     public String getGreeting() {
         return "こんにちは SEMonster";
-    }
+    }こんにちは SEMonster
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
